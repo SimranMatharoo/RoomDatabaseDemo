e62094d725c864b14080fecccad84ade3bac96d5
SimranMatharoo <simmatharoo2741@gmail.com>
2021-06-08 23:14:49 +0530

    App completed

--- app/src/main/java/com/example/roomdemoapp/UpdateTaskActivity.java
+++ app/src/main/java/com/example/roomdemoapp/UpdateTaskActivity.java
@@ -1,15 +1,165 @@
 package com.example.roomdemoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UpdateTaskActivity extends AppCompatActivity {
+
+    EditText editTextTask, editTextDesc, editTextFinishBy;
+    CheckBox checkBoxFinished;
+    Button btn_update;
+    Button btn_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_update_task);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        editTextFinishBy = findViewById(R.id.editTextFinishBy);
+        btn_update = findViewById(R.id.button_update);
+        btn_delete = findViewById(R.id.button_delete);
+
+        checkBoxFinished = findViewById(R.id.checkBoxFinished);
+
+        Task task = (Task)getIntent().getSerializableExtra("task");
+        loadTask(task);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Updated!", Toast.LENGTH_LONG).show();
+                updateTask(task);
+            }
+        });
+
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(UpdateTaskActivity.this);
+                builder.setTitle("Are you sure you want to delete?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteTask(task);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                AlertDialog ad = builder.create();
+                ad.show();
+
+
+
+            }
+        });
+
     }
+
+
+    //getting values of task from MainActivity
+    private void loadTask(Task task) {
+        editTextTask.setText(task.getTask());
+        editTextDesc.setText(task.getDesc());
+        editTextFinishBy.setText(task.getFinishBy());
+        checkBoxFinished.setChecked(task.isFinished());
+    }
+
+//updating the values of a task including the status of the task.
+    private void updateTask(final Task task) {
+        final String sTask = editTextTask.getText().toString().trim();
+        final String sDesc = editTextDesc.getText().toString().trim();
+        final String sFinishBy = editTextFinishBy.getText().toString().trim();
+
+        if (sTask.isEmpty()) {
+            editTextTask.setError("Task required");
+            editTextTask.requestFocus();
+            return;
+        }
+
+        if (sDesc.isEmpty()) {
+            editTextDesc.setError("Desc required");
+            editTextDesc.requestFocus();
+            return;
+        }
+
+        if (sFinishBy.isEmpty()) {
+            editTextFinishBy.setError("Finish by required");
+            editTextFinishBy.requestFocus();
+            return;
+        }
+
+        class UpdateTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                task.setTask(sTask);
+                task.setDesc(sDesc);
+                task.setFinishBy(sFinishBy);
+                task.setFinished(checkBoxFinished.isChecked());
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                        .taskDao()
+                        .update(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(UpdateTaskActivity.this, MainActivity.class));
+            }
+        }
+
+        UpdateTask ut = new UpdateTask();
+        ut.execute();
+    }
+
+//deleting the task
+    private void deleteTask(final Task task) {
+        class DeleteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                        .taskDao()
+                        .delete(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(UpdateTaskActivity.this, MainActivity.class));
+            }
+        }
+
+        DeleteTask dt = new DeleteTask();
+        dt.execute();
+
+    }
+
+
+
 }
